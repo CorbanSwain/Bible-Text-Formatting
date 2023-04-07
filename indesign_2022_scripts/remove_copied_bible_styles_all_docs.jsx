d5c6ef9c8f43ab6cc45cbc46c924cf2b1e48082d
Corban Swain <CorbanSwain@gmail.com>
2023-04-07 12:05:43 -0400

    adding additional indesign script

---
+++ indesign_2022_scripts/remove_copied_bible_styles_all_docs.jsx
@@ -0,0 +1,18 @@
+﻿main();
+
+function main() {
+    var allDocs = app.documents.everyItem();
+    
+    allDocs.paragraphStyles.item("Normal_rtf_1").remove("Normal");
+    
+    var removeParaArr = ["heading 2",  "heading 1", "footnote text"];
+    for (var i = 0; i < removeParaArr.length; ++i) {
+        allDocs.paragraphStyles.item(removeParaArr[i]).remove();
+    }
+    
+    var removeCharArr = ["footnote reference", "Verse"]
+    for (var i = 0; i < removeCharArr.length; ++i) {
+        allDocs.characterStyles.item(removeCharArr[i]).remove();
+    }
+
+ }
